af9e114178e3d17e8892dd85939c1d1ffb4f8369
gabrielcessil <gabrielcessil@gmail.com>
2025-02-03 13:30:55 -0300

    Better organized

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo -e "Download zips (do not extract):\n  openmpi-3.1.6\n  cmake-3.26.0\n  zlib-1.2.11\n  hdf5-1.8.12\n  silo-4.10.2\nThen, press any key to start..."
+echo -e "Press any key to start..."
 read -p ""
+
+
+# INCLUDE {#include "IO/silo.h"} on LBPM_source/IO/Reader.cpp headers
+echo -e "\n\n\n\n\n Please, write {#include \"IO/silo.h\"} on LBPM_source/IO/Reader.cpp headers.\n\n Then press any key to continue: ..."
+read -p ""
+
+# INCLUDE {#include <cstdint>} on LBPM_source/tests/DataAggregator.cpp headers
+echo -e "\n\n\n\n\n Please, write {#include <cstdint>} on LBPM_source/tests/DataAggregator.cpp headers.\n\n Then press any key to continue: ..."
+read -p ""
+
 
 sudo apt install gfortran
 
@@ -11,11 +21,14 @@ export MPI_DIR=$(pwd)/mpi
 export LBPM_ZLIB_DIR=$(pwd)/zlib
 export LBPM_HDF5_DIR=$(pwd)/hdf5
 export LBPM_SILO_DIR=$(pwd)/silo
+
+
 echo $MPI_DIR
 echo $LBPM_ZLIB_DIR
 echo $LBPM_HDF5_DIR
 echo $LBPM_SILO_DIR
-
+echo -e "\n\n Please, make sure the above paths are were properly in the installation folder. Then, press any key to continue: ..."
+read -p ""
 tar -xvzf openmpi-3.1.6.tar.gz
 cd openmpi-3.1.6
 ./configure --prefix=$MPI_DIR
@@ -75,7 +88,7 @@ echo $LBPM_DIR
 
 
 # DOWNLOAD LBPM, THEN EXTRACT AT LBPM_SOURCE FOLDER.
-echo -e "\n\n\n\n\n Cloning github repository, make sure you have it on your machine.\n\n Then press any key to continue ..."
+echo -e "\n\n\n\n\n Cloning github repository ... before this, make sure you have your git properly set in your machine.\n\n Then press any key to continue ..."
 read -p ""
 
 # prosseguir
@@ -89,18 +102,11 @@ cd ..
 
 cd $LBPM_DIR
 
-# INCLUDE {#include "IO/silo.h"} on LBPM_source/IO/Reader.cpp headers
-echo -e "\n\n\n\n\n Please, write {#include \"IO/silo.h\"} on LBPM_source/IO/Reader.cpp headers.\n\n Then press any key to continue: ..."
-read -p ""
-
-# INCLUDE {#include <cstdint>} on LBPM_source/tests/DataAggregator.cpp headers
-echo -e "\n\n\n\n\n Please, write {#include <cstdint>} on LBPM_source/tests/DataAggregator.cpp headers.\n\n Then press any key to continue: ..."
-read -p ""
 
 cmake                                           \
     -D CMAKE_BUILD_TYPE:STRING=Release          \
-    -D CMAKE_C_COMPILER:PATH=mpicc              \
-    -D CMAKE_CXX_COMPILER:PATH=mpicxx           \
+    -D CMAKE_C_COMPILER:PATH=$MPI_DIR/bin/mpicc              \
+    -D CMAKE_CXX_COMPILER:PATH=$MPI_DIR/bin/mpicxx           \
     -D CMAKE_C_FLAGS="-fPIC"                    \
     -D CMAKE_CXX_FLAGS="-fPIC"                  \
     -D CMAKE_CXX_STD=14                         \
@@ -118,7 +124,7 @@ make -j4
 
 make install
 
-ctest # Falhar alguns testes eh normal
+ctest
 
 echo -e "\n\nFinished. Some tests may fail, but it should still be usable. \n\n"
 
