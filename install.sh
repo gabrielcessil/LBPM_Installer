d77db811b7c26e12d6c5c55f25895778cd3ad783
Gabriel Silveira <48459677+gabrielcessil@users.noreply.github.com>
2026-03-25 14:48:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo -e "Press any key to start..."
-read -p ""
-
 DEFAULT_REPO="https://github.com/poro-labcc/lbpm/"
 DEFAULT_BRANCH="master"
 
@@ -16,7 +13,7 @@ export LBPM_HDF5_DIR=$(pwd)/hdf5
 export LBPM_SILO_DIR=$(pwd)/silo
 
 
-
+printf "\033c"
 echo -e "\n\n Please, make sure the following paths are correctly set in the installation folder. Then, press any key to continue..."
 echo $MPI_DIR
 echo $LBPM_ZLIB_DIR
@@ -24,14 +21,18 @@ echo $LBPM_HDF5_DIR
 echo $LBPM_SILO_DIR
 read -p ""
 
+printf "\033c"
+echo -e "\n Installing OpenMPI ..."
 tar -xvzf openmpi-3.1.6.tar.gz
 cd openmpi-3.1.6
 ./configure --prefix=$MPI_DIR
 make
 sudo make install
 
+printf "\033c"
+echo -e "\n\n OpenMPI successfully installed ..."
+echo -e "\n Installing CMake ..."
 cd ..
-
 tar -xvzf cmake-3.26.0.tar.gz
 cd cmake-3.26.0
 sudo apt-get install libgl1-mesa-dev
@@ -39,49 +40,53 @@ sudo apt-get install libgl1-mesa-dev
 ./bootstrap --prefix=/usr/local
 make
 
+printf "\033c"
+echo -e "\n\n CMake successfully installed ..."
+echo -e "\n Installing gcd ..."
 sudo apt install gcd $LBPM_DIR
-
 sudo make install
 
 cd ..
-
 tar -xzvf zlib-1.2.11.tar.gz
 tar -xzvf hdf5-1.8.12.tar.gz
 tar -xzvf silo-4.10.2.tar.gz
 
+printf "\033c"
+echo -e "\n\n GCD successfully installed ..."
+echo -e "\n Installing Zlib ..."
 cd zlib-1.2.11
-
 ./configure --prefix=$LBPM_ZLIB_DIR && make && make install
 
+printf "\033c"
+echo -e "\n\n Zlib successfully installed ..."
+echo -e "\n Installing hdf5 ..."
 cd ../hdf5-1.8.12
-
 CC=$MPI_DIR/bin/mpicc  CXX=$MPI_DIR/bin/mpicxx CXXFLAGS="-fPIC -O3 -std=c++14" \
 ./configure --prefix=$LBPM_HDF5_DIR --enable-parallel --enable-shared --with-zlib=$LBPM_ZLIB_DIR
  make && make install
 
-
+printf "\033c"
+echo -e "\n Installing ZLib ..."
 sudo apt-get install zlib1g-dev
 
+
+printf "\033c"
+echo -e "\n\n ZLib successfully installed ..."
+echo -e "\n Installing Silo ..."
 cd ../silo-4.10.2
-
-
 CC=$MPI_DIR/bin/mpicc  CXX=$MPI_DIR/bin/mpicxx CXXFLAGS="-fPIC -O3 -std=c++14" \
 ./configure --prefix=$LBPM_SILO_DIR -with-hdf5=$LBPM_HDF5_DIR/include,$LBPM_HDF5_DIR/lib --enable-static
  make && make install
 
 cd ..
-
-
-
 mkdir LBPM_source LBPM_dir
-
 export LBPM_SOURCE=$(pwd)/LBPM_source
 export LBPM_DIR=$(pwd)/LBPM_dir
 echo $LBPM_SOURCE
 echo $LBPM_DIR
 
 
-# DOWNLOAD LBPM, THEN EXTRACT AT LBPM_SOURCE FOLDER.
+printf "\033c"
 echo -e "\n\n\n\n\n Cloning github repository. Make sure you have git properly set in your machine. Consider using 'apt-get install git' ... \n\n Press any key to continue ..."
 read -p ""
 
